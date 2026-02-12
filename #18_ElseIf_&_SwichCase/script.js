84e14f0e3fd3514ba9cb3630467dfb1e66d2748c
ifantrijm <191655893+ifantrijm@users.noreply.github.com>
2026-02-12 21:23:10 +0700

    Add files via upload

---
+++ #18_ElseIf_&_SwichCase/script.js
@@ -0,0 +1,20 @@
+let nama = "ucup";
+
+if (nama === "otong") {
+    console.log("hai otong sigma");
+} else if(nama === "ucup") {
+    console.log("hai ucup sigma");
+} else {
+    console.log("anda siap?");
+}
+
+switch(nama){
+    case "otong":
+        console.log("switch case otong");
+        break
+    case "ucup":
+        console.log("swich case ucup");
+        break
+    default:
+        console.log("switch case nobody")
+}
